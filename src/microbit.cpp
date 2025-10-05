8cbc913b68582b3f7f21af1cc9f3bd920fce8dd2
Pythom1234 <tom.fucik@email.cz>
2025-10-05 10:56:37 +0200

    Add blank line after license header in all files

--- src/microbit.cpp
+++ src/microbit.cpp
@@ -17,6 +17,7 @@
  * License along with this library; if not, see
  * <http://www.gnu.org/licenses/>.
  */
+
 #include "microbit.h"
 #include "peripheral.h"
 #include <cmath>
--- src/startup.cpp
+++ src/startup.cpp
@@ -17,6 +17,7 @@
  * License along with this library; if not, see
  * <http://www.gnu.org/licenses/>.
  */
+
 #include "types.h"
 
 static u32* sp;
