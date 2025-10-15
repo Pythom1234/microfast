c6009d2124d279219fdd749ddeb374d461a06d6e
Pythom1234 <tom.fucik@email.cz>
2025-10-15 17:36:45 +0200

    Update toolchain paths to use Windows binaries

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 del build\\* /Q /F
 mkdir build
 
-toolchain\\bin\\linux\\clang \
+toolchain\\bin\\windows\\clang \
 --target=armv7m-none-eabi -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard \
 -nostdlib -nostdinc -nodefaultlibs -Oz -Iinc -nostartfiles \
 -fno-exceptions -fno-rtti -fno-unwind-tables -fno-asynchronous-unwind-tables -flto \
@@ -13,8 +13,8 @@ toolchain\\bin\\linux\\clang \
 -Ltoolchain\\stdlib\\lib\\nano \
 -lgcc -lc -lg -lstdc++ -lnosys -lm -o build\\main.elf
 
-toolchain\\bin\\linux\\llvm-objcopy -O ihex build\\main.elf build\\main.hex
+toolchain\\bin\\windows\\llvm-objcopy -O ihex build\\main.elf build\\main.hex
 
-toolchain\\bin\\linux\\llvm-objcopy -O binary build\\main.elf build\\main.bin
+toolchain\\bin\\windows\\llvm-objcopy -O binary build\\main.elf build\\main.bin
 
-toolchain\\bin\\linux\\pyocd flash build\\main.hex
+toolchain\\bin\\windows\\pyocd flash build\\main.hex
