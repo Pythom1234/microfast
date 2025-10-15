24117b51869d1b100ccd1e63a7104c58e87c10d9
Pythom1234 <tom.fucik@email.cz>
2025-10-15 17:31:13 +0200

    Add Windows toolchain binaries and build script
    Add make.bat for Windows build process Include clang, ld.lld,
    llvm-objcopy, and pyocd binaries Configure build for ARM Cortex-M4 with
    hard-float ABI Support ELF, HEX, and binary output formats Enable LTO
    and linker garbage collection Add automatic flashing with pyocd

---
+++ make.bat
@@ -0,0 +1,20 @@
+del build\\* /Q /F
+mkdir build
+
+toolchain\\bin\\linux\\clang \
+--target=armv7m-none-eabi -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard \
+-nostdlib -nostdinc -nodefaultlibs -Oz -Iinc -nostartfiles \
+-fno-exceptions -fno-rtti -fno-unwind-tables -fno-asynchronous-unwind-tables -flto \
+-ffunction-sections -fdata-sections -g0 -Wl,-T,linker.ld -Wl,--gc-sections \
+-Itoolchain\\stdlib\\include\\c++\\14.2.0\\arm-none-eabi\\thumb\\v7e-m+fp\\hard \
+-Itoolchain\\stdlib\\include\\c++\\14.2.0 \
+-Itoolchain\\stdlib\\include \
+-Itoolchain\\stdlib\\inc \
+-Ltoolchain\\stdlib\\lib\\nano \
+-lgcc -lc -lg -lstdc++ -lnosys -lm -o build\\main.elf
+
+toolchain\\bin\\linux\\llvm-objcopy -O ihex build\\main.elf build\\main.hex
+
+toolchain\\bin\\linux\\llvm-objcopy -O binary build\\main.elf build\\main.bin
+
+toolchain\\bin\\linux\\pyocd flash build\\main.hex
