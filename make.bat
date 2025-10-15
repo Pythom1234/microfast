a45b4480e11a5adad8d02a812b6a7f63471761e1
Pythom1234 <tom.fucik@email.cz>
2025-10-15 18:18:11 +0200

    Update make.bat to support multiple source files
    Use dynamic source file collection instead of hardcoded list Add proper
    batch script headers and delayed expansion Switch to LLD linker for
    better performance

--- make.bat
+++ make.bat
@@ -1,7 +1,14 @@
+@echo off
+setlocal enabledelayedexpansion
 mkdir build
 del /Q /F build\*
 
-toolchain\bin\windows\clang --target=armv7m-none-eabi -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -nostdlib -nostdinc -nodefaultlibs -Oz -Iinc -nostartfiles -fno-exceptions -fno-rtti -fno-unwind-tables -fno-asynchronous-unwind-tables -flto -ffunction-sections -fdata-sections -g0 -Wl,-T,linker.ld -Wl,--gc-sections -Itoolchain\stdlib\include\c++\14.2.0\arm-none-eabi\thumb\v7e-m+fp\hard -Itoolchain\stdlib\include\c++\14.2.0 -Itoolchain\stdlib\include -Itoolchain\stdlib\inc -Ltoolchain\stdlib\lib\nano -lgcc -lc -lg -lstdc++ -lnosys -lm -o build\main.elf src\main.cpp src\microbit.cpp src\startup.cpp src\startup.s
+set SRC_FILES=
+for %%f in (src\*.c src\*.cpp src\*.s) do (
+    set SRC_FILES=!SRC_FILES! %%f
+)
+
+toolchain\bin\windows\clang -fuse-ld=lld --target=armv7m-none-eabi -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -nostdlib -nostdinc -nodefaultlibs -Oz -Iinc -nostartfiles -fno-exceptions -fno-rtti -fno-unwind-tables -fno-asynchronous-unwind-tables -flto -ffunction-sections -fdata-sections -g0 -Wl,-T,linker.ld -Wl,--gc-sections -Itoolchain\stdlib\include\c++\14.2.0\arm-none-eabi\thumb\v7e-m+fp\hard -Itoolchain\stdlib\include\c++\14.2.0 -Itoolchain\stdlib\include -Itoolchain\stdlib\inc -Ltoolchain\stdlib\lib\nano -lgcc -lc -lg -lstdc++ -lnosys -lm -o build\main.elf !SRC_FILES!
 
 toolchain\bin\windows\llvm-objcopy -O ihex build\main.elf build\main.hex
 toolchain\bin\windows\llvm-objcopy -O binary build\main.elf build\main.bin
